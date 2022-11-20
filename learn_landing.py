ad14ee76ab9b9c8f557a75a64b92b3547c08077a
brooky56 <aidarahmetshin56@gmail.com>
2022-11-20 15:58:33 +0300

    remove dependences

--- learn_landing.py
+++ learn_landing.py
@@ -1,5 +1,6 @@
 import numpy as np
-from stable_baselines3 import PPO, SAC
+import torch
+from stable_baselines3 import PPO, SAC, TD3 
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.callbacks import BaseCallback
@@ -39,47 +40,90 @@ class HParamCallback(BaseCallback):
         return True
 
 
-TOTAL_TIMESTAMPS = 1_000_000
+TOTAL_TIMESTAMPS = 400000
 OBS = ObservationType.KIN
-ACT = ActionType.RPM
+ACT = ActionType.ONE_D_RPM
 
 env = make_vec_env("landing-aviary-v0", n_envs=7,
                    env_kwargs={'obs': OBS, 'act': ACT})
-try:
-    model_name = "landing-SAC_kin_tt200000"
-    model = SAC.load(model_name, env=env)
-    # model = PPO.load(model_name, env=env)
-except:
-    policy = "MlpPolicy" if OBS == ObservationType.KIN else "CnnPolicy"
-    model = SAC(
-        policy,
-        env,
-        buffer_size=100000,
-        batch_size=64,
-        learning_rate=0.001,
-        tensorboard_log="./tensorboard/",
-        seed=0,
-        verbose=1
-    )
-    # model = PPO(
-    #         policy,
-    #         env,
-    #         # learning_rate = 0.001,
-    #         batch_size=64,
-    #         n_epochs = 5,
-    #         n_steps = 64,
-    #         ent_coef = 0.01,
-    #         tensorboard_log="./tensorboard/",
-    #         seed=0,
-    #         verbose=1
-    #         )
 
-model.learn(total_timesteps=TOTAL_TIMESTAMPS, callback=HParamCallback())
-model_name = "landing-" + model.__class__.__name__ + \
-    "_" + OBS._value_ + "_tt" + str(TOTAL_TIMESTAMPS)
-model.save(model_name)
-mean_reward, std_reward = evaluate_policy(
-    model, env, n_eval_episodes=10, deterministic=False)
-print("------------------------------------------------")
-print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
-print("------------------------------------------------")
+env_td3 = make_vec_env("landing-aviary-v0", n_envs=1,
+                   env_kwargs={'obs': OBS, 'act': ACT})
+
+
+EXPEREMENT_PARAMS = {
+    "PPO":[ {"name":"PPO_1", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.99},
+            {"name":"PPO_2", "buffer_size":100000, "batch_size":64, "learning_rate":0.0001, "gamma":0.9},
+            {"name":"PPO_3", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.1},
+            {"name":"PPO_4", "buffer_size":100000, "batch_size":32, "learning_rate":0.001, "gamma":0.7}], 
+    "TD3":[{"name":"DDPG_1", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.99},
+            {"name":"DDPG_2", "buffer_size":100000, "batch_size":64, "learning_rate":0.0001, "gamma":0.99},
+            {"name":"DDPG_3", "buffer_size":100000, "batch_size":64, "learning_rate":0.01, "gamma":0.7},
+            {"name":"DDPG_4", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.7}], 
+    "SAC":[ {"name":"SAC_1", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.99}, 
+            {"name":"SAC_2", "buffer_size":100000, "batch_size":64, "learning_rate":0.0001, "gamma":0.9}, 
+            {"name":"SAC_3", "buffer_size":100000, "batch_size":32, "learning_rate":0.01, "gamma":0.1},
+            {"name":"SAC_4", "buffer_size":100000, "batch_size":64, "learning_rate":0.001, "gamma":0.7}]    
+}
+
+policy = "MlpPolicy" if OBS == ObservationType.KIN else "CnnPolicy"
+
+offpolicy_kwargs = dict(activation_fn=torch.nn.ReLU,
+                            net_arch=[512, 512, 256, 128]
+                            )
+
+for model_type in EXPEREMENT_PARAMS:
+    experements = EXPEREMENT_PARAMS[model_type]
+    for exp in experements:
+        model_name = "lan_{0}_kin".format(exp["name"])
+        
+        # # On-policy algorithms
+        if model_type == "PPO":
+            model = PPO(
+                    policy,
+                    env,
+                    learning_rate=exp["learning_rate"],
+                    gamma=exp["gamma"],
+                    batch_size=exp["batch_size"],
+                    n_epochs = 5,
+                    n_steps = 64,
+                    ent_coef = 0.01,
+                    tensorboard_log="./tensorboard/",
+                    seed=0,
+                    verbose=1
+                )
+        # Off-policy algorithms
+        elif model_type == "SAC":
+            model = SAC(
+                policy,
+                env,
+                buffer_size=exp["buffer_size"],
+                batch_size=exp["batch_size"],
+                learning_rate=exp["learning_rate"],
+                gamma=exp["gamma"],
+                tensorboard_log="./tensorboard/",
+                seed=0,
+                verbose=1
+            )
+        elif model_type == "DDPG":
+            model = TD3(
+                policy, 
+                env_td3,
+                buffer_size=exp["buffer_size"],
+                batch_size=exp["batch_size"],
+                learning_rate=exp["learning_rate"],
+                gamma=exp["gamma"],
+                tensorboard_log="./tensorboard/",
+                seed=0,
+                verbose=1
+            )
+
+        model.learn(total_timesteps=TOTAL_TIMESTAMPS, callback=HParamCallback())
+        model_name = "landing-" + model.__class__.__name__ + \
+            "_" + OBS._value_ + "_tt" + str(TOTAL_TIMESTAMPS) + model_name
+        model.save(model_name)
+        mean_reward, std_reward = evaluate_policy(
+            model, env, n_eval_episodes=10, deterministic=False)
+        print("------------------------------------------------")
+        print(f"mean_reward={mean_reward:.2f} +/- {std_reward}")
+        print("------------------------------------------------")
